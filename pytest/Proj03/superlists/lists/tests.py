1e61cecd45334d7f812b6b347b1d3a71ddb5075b
bio-tarik <bio.tarik@gmail.com>
2017-07-30 16:23:51 +0200

    Creating a really basic view

--- pytest/Proj03/superlists/lists/tests.py
+++ pytest/Proj03/superlists/lists/tests.py
@@ -3,6 +3,8 @@
 """
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -10,11 +12,22 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     """
-        asdf
+        "Lists page" test cases
     """
     def test_root_url_resolves_to_home_page_view(self):
         """
-            Insert docstring
+            Checks if root page redirects correctly to the home page view
         """
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """
+            Parses the obtained html and check wether its contents matches
+            that from the homepage
+        """
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
