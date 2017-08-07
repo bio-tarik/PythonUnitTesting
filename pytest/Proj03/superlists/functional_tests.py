cdd8babe882925ca4b03548ff67daf7145e72953
bio-tarik <bio.tarik@gmail.com>
2017-08-07 08:52:56 -0300

    Redirecting after form submit and displaying multiples items on the items table

--- pytest/Proj03/superlists/functional_tests.py
+++ pytest/Proj03/superlists/functional_tests.py
@@ -3,7 +3,7 @@
     Tutorial: https://www.obeythetestinggoat.com/book/praise.harry.html
 """
 
-import unittest
+import unittest, time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -46,22 +46,26 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        time.sleep(1)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
