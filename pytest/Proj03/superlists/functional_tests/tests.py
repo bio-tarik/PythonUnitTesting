bd691aa8578acd3d64590744da58be05be102b57
bio-tarik <bio.tarik@gmail.com>
2017-08-09 06:03:41 -0300

    Functional test became an app + using django's LiverServerTestCase to create / destroy the database

--- pytest/Proj03/superlists/functional_tests/tests.py
+++ pytest/Proj03/superlists/functional_tests/tests.py
@@ -3,12 +3,13 @@
     Tutorial: https://www.obeythetestinggoat.com/book/praise.harry.html
 """
 
-import unittest, time
+import time
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """
         To-Do list homepage test class
     """
@@ -25,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         """
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -70,7 +71,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-
-if __name__ == '__main__':
-    unittest.main()
