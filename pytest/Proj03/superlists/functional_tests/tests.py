45b52c99a7862e0dc6abaabed33a65fa9a302484
bio-tarik <bio.tarik@gmail.com>
2017-09-16 13:44:27 -0300

    Using bootstrap to improve layout

--- pytest/Proj03/superlists/functional_tests/tests.py
+++ pytest/Proj03/superlists/functional_tests/tests.py
@@ -4,14 +4,14 @@
 """
 
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """
         To-Do list homepage test class
     """
